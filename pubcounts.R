eeed51c1a055b2bcd5cd0fb09387b6e020d08ff6
Rajarshi Guha <rajarshi.guha@gmail.com>
2016-09-25 22:09:17 -0400

    updated R code

--- pubcounts.R
+++ pubcounts.R
@@ -3,6 +3,7 @@ library(reshape2)
 x <- read.csv('pubcounts.csv', header=TRUE)
 ggplot(x, aes(x=as.factor(year),y=cdk2))+
   geom_bar(stat='identity', fill='beige', colour='black', position='dodge')+
-  geom_text(aes(label=cdk2,x=as.factor(year),y=cdk2),
+  geom_text(aes(label=cdk2,x=as.factor(year),y=cdk2, ymax=cdk2),vjust=1.25,
             position = position_dodge(width=1))+
-  xlab("Year")+ylab("Number of citations")
+  xlab("Year")+ylab("Number of citations")#+ylim(c(0, 60))
+ggsave("cdk-paper-citation-growth.png")
